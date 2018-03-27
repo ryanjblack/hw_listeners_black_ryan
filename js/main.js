21a8ed971128ded60c74b5c47edd3367e546bb04
Ryan Black <ryan.j.black@ufl.edu>
2018-03-27 15:41:43 -0400

    optimized the js code, short hand

--- js/main.js
+++ js/main.js
@@ -1,39 +1,14 @@
 var nameInput = document.getElementById('fname');
+var nameInput2 = document.getElementById('lname');
+var nameInput3 = document.getElementById('email');
+var nameInput4 = document.getElementById('msg');
 
-document.querySelector('form.pure-form').addEventListener('submit', function (e) {
+document.querySelector('form.form').addEventListener('submit', function (e) {
 
-    //prevent the normal submission of the form
     e.preventDefault();
 
     console.log('The first name is:' + ' ' + nameInput.value);
-});
-
-var nameInput2 = document.getElementById('lname');
-
-document.querySelector('form.pure-form').addEventListener('submit', function (e) {
-
-    //prevent the normal submission of the form
-    e.preventDefault();
-
     console.log('The last name is:' + ' ' + nameInput2.value);
-});
-
-var nameInput3 = document.getElementById('email');
-
-document.querySelector('form.pure-form').addEventListener('submit', function (e) {
-
-    //prevent the normal submission of the form
-    e.preventDefault();
-
     console.log('The email is:' + ' ' + nameInput3.value);
-});
-
-var nameInput4 = document.getElementById('msg');
-
-document.querySelector('form.pure-form').addEventListener('submit', function (e) {
-
-    //prevent the normal submission of the form
-    e.preventDefault();
-
     console.log('The message is:' + ' ' + nameInput4.value);
 });
