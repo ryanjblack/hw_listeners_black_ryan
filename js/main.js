41864574744fa9886095b641483fe9cc530d2273
Ryan Black <ryan.j.black@ufl.edu>
2018-03-27 15:39:09 -0400

    completed form and listeners, long form method

--- js/main.js
+++ js/main.js
@@ -1,13 +1,39 @@
-var pageHeading = document.getElementById('pageHeading');
+var nameInput = document.getElementById('fname');
 
-var n = 0;
-function increment() {
-    n++;
-    return n;
-}
+document.querySelector('form.pure-form').addEventListener('submit', function (e) {
 
-pageHeading.addEventListener('click', function () {
-    var myElement = document.createElement('p');
-    myElement.innerHTML = 'This is click number:' + ' ' + increment();
-    document.getElementById('pageHeading').appendChild(myElement);
+    //prevent the normal submission of the form
+    e.preventDefault();
+
+    console.log('The first name is:' + ' ' + nameInput.value);
+});
+
+var nameInput2 = document.getElementById('lname');
+
+document.querySelector('form.pure-form').addEventListener('submit', function (e) {
+
+    //prevent the normal submission of the form
+    e.preventDefault();
+
+    console.log('The last name is:' + ' ' + nameInput2.value);
+});
+
+var nameInput3 = document.getElementById('email');
+
+document.querySelector('form.pure-form').addEventListener('submit', function (e) {
+
+    //prevent the normal submission of the form
+    e.preventDefault();
+
+    console.log('The email is:' + ' ' + nameInput3.value);
+});
+
+var nameInput4 = document.getElementById('msg');
+
+document.querySelector('form.pure-form').addEventListener('submit', function (e) {
+
+    //prevent the normal submission of the form
+    e.preventDefault();
+
+    console.log('The message is:' + ' ' + nameInput4.value);
 });
