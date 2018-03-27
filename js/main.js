314f6b19ef0c44a9fc5a0b37f81f18367b0fee8e
Ryan Black <ryan.j.black@ufl.edu>
2018-03-27 18:32:28 -0400

    custom text based on 5 listening events

--- js/main.js
+++ js/main.js
@@ -1,14 +1,49 @@
-var nameInput = document.getElementById('fname');
-var nameInput2 = document.getElementById('lname');
-var nameInput3 = document.getElementById('email');
-var nameInput4 = document.getElementById('msg');
+//  Listening for a mouse click, adds an element
 
-document.querySelector('form.form').addEventListener('submit', function (e) {
+var pageHeading = document.getElementById('cus1');
 
-    e.preventDefault();
+pageHeading.addEventListener('click', function () {
+    var myElement = document.createElement('p');
+    myElement.innerHTML = 'Each time you click it adds some text.';
+    document.getElementById('cus1').appendChild(myElement);
+});
 
-    console.log('The first name is:' + ' ' + nameInput.value);
-    console.log('The last name is:' + ' ' + nameInput2.value);
-    console.log('The email is:' + ' ' + nameInput3.value);
-    console.log('The message is:' + ' ' + nameInput4.value);
+//  Listening for a mouse click, adds an element
+
+var pageHeading2 = document.getElementById('cus2');
+
+pageHeading2.addEventListener('wheel', function () {
+    var myElement = document.createElement('p');
+    myElement.innerHTML = 'Each time you scroll your mouse over this block it adds some text.';
+    document.getElementById('cus2').appendChild(myElement);
+});
+
+//  Listening for a double mouse click, adds an element
+
+var pageHeading3 = document.getElementById('cus3');
+
+pageHeading3.addEventListener('dblclick', function () {
+    var myElement = document.createElement('p');
+    myElement.innerHTML = 'Each time you double click me it adds some text.';
+    document.getElementById('cus3').appendChild(myElement);
+});
+
+//  Listening for the content to be copied to the user's clipboard, adds an element
+
+var pageHeading4 = document.getElementById('cus4');
+
+pageHeading4.addEventListener('copy', function () {
+    var myElement = document.createElement('p');
+    myElement.innerHTML = 'Each time you copy this section, it adds some text.';
+    document.getElementById('cus4').appendChild(myElement);
+});
+
+//  Listening for the mouse to over over the text, adds an element
+
+var pageHeading5 = document.getElementById('cus5');
+
+pageHeading5.addEventListener('mouseenter', function () {
+    var myElement = document.createElement('p');
+    myElement.innerHTML = 'Each time you hover over me it adds some text.';
+    document.getElementById('cus5').appendChild(myElement);
 });
